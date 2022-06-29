a110cbc9e0cedadbf3223d39a25e408c626bb671
Anzaiyasuto <al18011@shibaura-it.ac.jp>
2022-06-29 11:04:35 +0900

    Initial Commit

---
+++ Test_connect_yasuto_18011/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_connect_yasuto_18011
+//
+//  Created by AnzaiYasuto al18011 on 2022/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
